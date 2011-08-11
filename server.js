40a25413b0876dd53546b48aa0f5faa1d5a78684
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.com>
2011-08-12 00:05:43 +0400

    Port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var app = module.exports = require('railway').createServer();
 
 if (!module.parent) {
-    app.listen(process.env.PORT || 3000);
+    app.listen(process.env.PORT || 1616);
     console.log("Railway server listening on port %d within %s environment", app.address().port, app.settings.env);
 }
 
